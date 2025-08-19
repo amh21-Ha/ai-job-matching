81281a8c8e93e7101e4fe1ba638991229c61dfc2
am <amha.ha07@gmail.com>
2025-08-19 22:03:54 +0300

    vercels

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from resume_match.matcher import ResumeMatcher
 from interview_ai.answer_eval import AnswerEvaluator
+import os
 
 app = FastAPI()
+
+# Path to the favicon.ico file
+favicon_path = os.path.join(os.path.dirname(__file__), "static", "favicon.ico")
+
+# Mount the "static" directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
 matcher = ResumeMatcher()
 evaluator = AnswerEvaluator()
 
